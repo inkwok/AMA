ec90709c6147ea45c1348f4465030dd21c7d45ec
inkwok <cerealprawn332@gmail.com>
2024-08-24 19:08:30 -0700

    Sad

---
+++ 3: Formatted IO/Exercises/prog4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float x;
+    int i, j;
+    printf("lmao: ");
+    scanf("%d%f%d", &i, &x, &j);
+    printf("%d\n%f\n%d\n", i, x, j);
+    return 0;
+}
